ebc01630a8357b992083f1d535fa4f9f7c0e6d66
moyuu-az <moyuu-az@users.noreply.github.com>
2022-12-17 10:03:50 +0000

    deploy: 0f85eae374a27edcdfb3936cc488f7e7ab8759e5

--- _next/static/9J1Wvi5PJXQBKTIy_Gc3P/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-837da0da4a77783f.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],"/linkSample":["static/chunks/pages/linkSample-8114cdd2a469db7b.js"],sortedPages:["/","/_app","/_error","/linkSample"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
