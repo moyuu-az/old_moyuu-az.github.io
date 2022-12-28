334db001829071073227ebce27e00bd5a5b1ef68
moyuu <sirius.nasa1000@gmail.com>
2022-12-29 01:01:36 +0900

    タイムラインをトップページに追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/global.css'
-import "./timeline/timeline.css";
+import '../styles/timeline.css'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,30 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
+
+const data = [
+    {
+        title: 2020,
+        content:
+            "test",
+    },
+    {
+        title: 2019,
+        content:
+            "test",
+    },
+    {
+        title: 2018,
+        content:
+            "test",
+    },
+    {
+        title: 2017,
+        content:
+            "test",
+    },
+];
+
 export default function Home() {
     return (
         <Layout home>
@@ -15,6 +39,22 @@ export default function Home() {
                     <a href="/posts/first-post">適当なポスト</a>.)
                 </p>
             </section>
+            <div className="timeline">
+                {data.map((bloc, index) => {
+                    return <TimeLineBloc data={bloc} direction={"right"} key={index} />;
+                })}
+            </div>
         </Layout>
     )
+}
+
+function TimeLineBloc({ data, direction }) {
+    return (
+        <div className={`container ${direction}`}>
+            <article className="content">
+                <h2>{data.title}</h2>
+                <p>{data.content}</p>
+            </article>
+        </div>
+    );
 }
